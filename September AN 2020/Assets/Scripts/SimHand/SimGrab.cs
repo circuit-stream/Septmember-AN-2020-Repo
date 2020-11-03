916c53ba46e46acd5cc248d1c71a5e8173ab8747
usmanmirCS <47401816+usmanmirCS@users.noreply.github.com>
2020-11-03 03:33:38 -0700

    Session 10

--- September AN 2020/Assets/Scripts/SimHand/SimGrab.cs
+++ September AN 2020/Assets/Scripts/SimHand/SimGrab.cs
@@ -6,15 +6,53 @@ public class SimGrab : MonoBehaviour
 {
     public Animator m_anim;
 
+    private GameObject m_collidingObject;
+    private GameObject m_heldObject;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Interactable")
+        {
+            m_collidingObject = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        m_collidingObject = null;
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
             m_anim.SetBool("isGrabbing", true);
+            if(m_collidingObject)
+            {
+                Grab();
+            }
         }
         else if(Input.GetKeyUp(KeyCode.Mouse1))
         {
             m_anim.SetBool("isGrabbing", false);
+            if(m_heldObject)
+            {
+                Release();
+            }
         }
+    }
+
+    void Grab()
+    {
+        m_heldObject = m_collidingObject;
+        m_heldObject.GetComponent<Rigidbody>().isKinematic = true;
+        m_heldObject.transform.SetParent(transform);
+    }
+
+    void Release()
+    {
+        m_heldObject.transform.SetParent(null);
+        m_heldObject.GetComponent<Rigidbody>().isKinematic = false;
+        m_heldObject = null;
     }
 }
---
+++ September AN 2020/Assets/Scripts/VR/VRGrab.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRGrab : MonoBehaviour
+{
+    public string m_gripAxisName;
+    private bool m_gripHeld;
+
+    public Animator m_anim;
+
+    void Update()
+    {
+        if (Input.GetAxis(m_gripAxisName) > 0.8f && m_gripHeld == false)
+        {
+            m_gripHeld = true;
+            m_anim.SetBool("isGrabbing", true);
+        }
+        else if (Input.GetAxis(m_gripAxisName) < 0.8f && m_gripHeld == true)
+        {
+            m_gripHeld = false;
+            m_anim.SetBool("isGrabbing", false);
+        }
+    }
+}
