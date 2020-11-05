4d01c08b123ed0f2b3e6c40d3e7ade94126cd0ef
usmanmirCS <47401816+usmanmirCS@users.noreply.github.com>
2020-11-04 18:30:58 -0700

    Session 11

---
+++ September AN 2020/Assets/Scripts/Interactions/TouchButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TouchButton : MonoBehaviour
+{
+    public Transform m_buttonMesh;
+    public Transform m_down;
+
+    public UnityEvent m_buttonPressed;
+    public UnityEvent m_buttonReleased;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            m_buttonMesh.position = m_down.position;
+            m_buttonPressed.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            m_buttonMesh.position = transform.position;
+            m_buttonReleased.Invoke();
+        }
+    }
+}
--- September AN 2020/Assets/Scripts/VR/VRGrab.cs
+++ September AN 2020/Assets/Scripts/VR/VRGrab.cs
@@ -9,17 +9,56 @@ public class VRGrab : MonoBehaviour
 
     public Animator m_anim;
 
+    private GameObject m_collidingObject;
+    private GameObject m_heldObject;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Interactable")
+        {
+            m_collidingObject = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        m_collidingObject = null;
+    }
+
     void Update()
     {
         if (Input.GetAxis(m_gripAxisName) > 0.8f && m_gripHeld == false)
         {
             m_gripHeld = true;
             m_anim.SetBool("isGrabbing", true);
+            if(m_collidingObject)
+            {
+                Grab();
+            }
         }
         else if (Input.GetAxis(m_gripAxisName) < 0.8f && m_gripHeld == true)
         {
             m_gripHeld = false;
             m_anim.SetBool("isGrabbing", false);
+            if(m_heldObject)
+            {
+                Release();
+            }
         }
     }
+
+    void Grab()
+    {
+        m_heldObject = m_collidingObject;
+        m_heldObject.GetComponent<Rigidbody>().isKinematic = true;
+        m_heldObject.transform.SetParent(transform);
+    }
+
+    void Release()
+    {
+        m_heldObject.transform.SetParent(null);
+        m_heldObject.GetComponent<Rigidbody>().isKinematic = false;
+        m_heldObject = null;
+    }
 }
+
