955c30908bdc46235560e5611f54305abb1a8779
usmanmirCS <47401816+usmanmirCS@users.noreply.github.com>
2020-11-16 18:18:36 -0700

    Session 14

---
+++ September AN 2020/Assets/Scripts/SimHand/SimTeleport.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class SimTeleport : MonoBehaviour
+{
+    private LineRenderer m_myLine;
+    private RaycastHit m_hit;
+    private bool m_canTeleport;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_myLine = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.T))
+        {
+            if (Physics.Raycast(transform.position, transform.forward, out m_hit))
+            {
+                m_myLine.enabled = true;
+                m_myLine.SetPosition(0, transform.position);
+                m_myLine.SetPosition(1, m_hit.point);
+                m_canTeleport = true;
+            }
+            else
+            {
+                m_myLine.enabled = false;
+                m_canTeleport = false;
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.T))
+        {
+            m_myLine.enabled = false;
+            if (m_canTeleport)
+            {
+                m_canTeleport = false;
+                transform.position = m_hit.point;
+            }
+        }
+    }
+}
---
+++ September AN 2020/Assets/Scripts/VR/VRTeleport.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRTeleport : MonoBehaviour
+{
+    public Transform m_vrRig;
+    public Transform m_vrCamera;
+    public string m_teleportButton;
+
+    private LineRenderer m_myLine;
+    private RaycastHit m_hit;
+    private bool m_canTeleport;
+    private Vector3 m_difference;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_myLine = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton(m_teleportButton))
+        {
+            if(Physics.Raycast(transform.position, transform.forward, out m_hit, 100))
+            {
+                m_myLine.enabled = true;
+                m_myLine.SetPosition(0, transform.position);
+                m_myLine.SetPosition(1, m_hit.point);
+                m_canTeleport = true;
+            }
+            else
+            {
+                m_canTeleport = false;
+                m_myLine.enabled = false;
+            }
+        }
+        else if(Input.GetButtonUp(m_teleportButton))
+        {
+            m_myLine.enabled = false;
+            if(m_canTeleport)
+            {
+                m_canTeleport = false;
+
+                m_difference = m_vrRig.position - m_vrCamera.localPosition;
+                m_difference.y = 0;
+
+                m_vrRig.position = m_hit.point + m_difference;
+            }
+        }
+    }
+}
